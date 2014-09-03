b76af9d444627cb6f3aae5bbf55ccbc96be2bce9
root <neginsky58@gmail.com>
2014-09-04 02:57:52 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PdfFiller::Application.config.secret_key_base = '81abb01869057735716e907d4555654e1907b8bb2bc7f4dcdce79599d5e8b81a85a320f5cb0dccc10db98ba5f0f7674b22da0b89256007c065101f21825b7f90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PdfFiller::Application.config.session_store :cookie_store, key: '_pdf_filler_session'
