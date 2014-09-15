53fc17f485c311a72264840c049347fa53425b4e
root <neginsky58@gmail.com>
2014-09-16 02:14:35 +0400

    pdftk path

--- app/pdfs/fillable_pdf_form.rb
+++ app/pdfs/fillable_pdf_form.rb
@@ -37,8 +37,8 @@ class FillablePdfForm
   end
 
   def pdftk
-    #@pdftk ||= PdfForms.new(ENV['PDFTK_PATH'] || '/opt/pdflabs/pdftk/bin/pdftk') # On my Mac, the location of pdftk was different than on my linux server.
-    @pdftk ||= PdfForms.new('/usr/bin/pdftk') # On my Mac, the location of pdftk was different than on my linux server.
+    @pdftk ||= PdfForms.new(ENV['PDFTK_PATH'] || '/opt/pdflabs/pdftk/bin/pdftk') # On my Mac, the location of pdftk was different than on my linux server.
+    #@pdftk ||= PdfForms.new('/usr/bin/pdftk') # On my Mac, the location of pdftk was different than on my linux server.
   end
 
   def fill_out
